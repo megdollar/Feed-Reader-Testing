c52fe98c7d34938f9ef7ff670624efb9e75f95ce
Megan Dollar <megdollar@gmail.com>
2017-01-06 14:58:00 -0500

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -59,7 +59,6 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
-        var spyEvent;
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
